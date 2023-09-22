d3dd09341f2727ec063da26a8a18ecc051993b46
RealYozha <ItzYozha@ya.ru>
2023-09-22 17:19:47 +0300

    1.2553

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 let moment = require('moment');
-let Now = moment()
 let MyBirthday = moment(new Date(2013, 1, 26));
 let MyNextBirthday = moment(new Date(2024, 1, 26));
 
-let Result = moment().diff(MyNextBirthday, 'days');
-let NextBirthdayAge = MyBirthday.diff(MyNextBirthday, 'years');
-let Age = MyBirthday.diff(Now, 'years');
-
 console.log(`Ты родился ${MyBirthday}`);
-console.log(`Тебе ${Math.abs(Age)} лет`);
-console.log(`Через ${Math.abs(Result)} дней тебе будет ${NextBirthdayAge}`);
+console.log(`Тебе ${Math.abs(MyBirthday.diff(moment(), 'years'))} лет`);
+console.log(`Через ${Math.abs(moment().diff(MyNextBirthday, 'days'))} дней тебе будет ${Math.abs(MyBirthday.diff(MyNextBirthday, 'years'))}`);
